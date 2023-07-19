b309d089c443e37b037ac0d9ee71fb56ac2b8a15
GNZ-TRESOR <tganza2002@gmail.com>
2023-07-19 10:24:58 +0200

    Edited README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-project
+
+in this project we are going to focus on how to make a game mostly loved by children of rock paper sissors and in this project we will introoduce in some basic concept of java script.
---
+++ project.js
@@ -0,0 +1,21 @@
+function getcomputerchoice(){
+    switch(choice){
+        case 1:
+            return(rock);
+
+            case 2:
+                return(paper);
+
+                case 3:
+                    return(scissors);
+    }
+}
+
+function playround(playerselection,computerselection){
+  
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+}
